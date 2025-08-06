8002b9c42253c79e32a994af72652be934a7c110
kkevinhu <kevinhu518007@gmail.com>
2025-08-06 13:42:14 +0800

    update

--- src/Controller.v
+++ src/Controller.v
@@ -4,7 +4,7 @@ module Controller(
     input [23:0] D_out,
     input        b,
     output       stall,
-    output       next_pc_sel,
+    output reg   next_pc_sel,
     // Fetch
     output [3:0] F_im_w_en,
     // Decode
@@ -22,10 +22,10 @@ module Controller(
     // Memory
     output reg [3:0] M_dm_w_en,
     // Write back
-    output       W_wb_en,
+    output reg   W_wb_en,
     output [4:0] W_rd_index,
-    output [2:0] W_f3,
-    output       W_wb_data_sel
+    output [2:0] W_f3_out,
+    output reg   W_wb_data_sel
 );
 
 reg [4:0] E_op, M_op, W_op;
@@ -42,6 +42,13 @@ wire is_E_rs2_W_rd_overlap, is_E_rs2_M_rd_overlap, is_E_use_rs2;
 
 wire is_M_use_rd;
 wire is_W_use_rd;
+
+wire is_DE_overlap, is_D_rs1_E_rd_overlap, is_D_rs2_E_rd_overlap;
+
+assign stall = (E_op ==  5'b00000) & is_DE_overlap;
+assign is_DE_overlap = (is_D_rs1_E_rd_overlap | is_D_rs2_E_rd_overlap);
+assign is_D_rs1_E_rd_overlap = is_D_use_rs1 & (D_out[17:13] == E_rd) & E_rd != 0;
+assign is_D_rs2_E_rd_overlap = is_D_use_rs2 & (D_out[22:18] == E_rd) & E_rd != 0;
 
 assign is_M_use_rd = (M_op == 5'b01000 || M_op == 5'b11000) ? 1'b0 : 1'b1;
 assign is_W_use_rd = (W_op == 5'b01000 || W_op == 5'b11000) ? 1'b0 : 1'b1;
@@ -69,6 +76,9 @@ assign E_rs2_data_sel        = is_E_rs2_M_rd_overlap ? 2'd1 : is_E_rs2_W_rd_over
 assign is_E_rs2_W_rd_overlap = is_E_use_rs2 & is_W_use_rd & (E_rs2 == W_rd) & W_rd != 0;
 assign is_E_rs2_M_rd_overlap = is_E_use_rs2 & is_M_use_rd & (E_rs2 == M_rd) & M_rd != 0;
 assign is_E_use_rs2          = (E_op == 5'b01100 || E_op == 5'b01000 || E_op == 5'b11000) ? 1'b0 : 1'b1;
+
+assign W_rd_index = W_rd;
+assign W_f3_out   = W_f3;
 
 always @(posedge clk or posedge rst) begin
     if (rst) begin
@@ -104,46 +114,55 @@ end
 always @(*) begin
     case (E_op) 
         5'b01100 : begin        // R type
-            E_jb_op1_sel <= 1'bx;
+            next_pc_sel   <= 1'b1;
+            E_jb_op1_sel  <= 1'bx;
             E_alu_op1_sel <= 0;
             E_alu_op2_sel <= 0;
         end
         5'b00100 : begin        // immediate
+            next_pc_sel   <= 1'b1;
             E_jb_op1_sel <= 1'bx;
             E_alu_op1_sel <= 0;
             E_alu_op2_sel <= 1;
         end
         5'b00000 : begin         // load
+            next_pc_sel   <= 1'b1;
             E_jb_op1_sel <= 1'bx;
             E_alu_op1_sel <= 0;
             E_alu_op2_sel <= 1;
         end
         5'b11001 : begin       // jalr
+            next_pc_sel   <= 1'b0;
             E_jb_op1_sel <= 0;
             E_alu_op1_sel <= 1;
             E_alu_op2_sel <= 1'bx; 
         end
         5'b01000 : begin       // store
+            next_pc_sel   <= 1'b1;
             E_jb_op1_sel <= 1'bx;
             E_alu_op1_sel <= 0;
             E_alu_op2_sel <= 1;
         end
         5'b11000 : begin      // branch
+            next_pc_sel   <= !b;
             E_jb_op1_sel <= 1'b1;
             E_alu_op1_sel <= 0;
             E_alu_op2_sel <= 0;
         end
         5'b01101 : begin      // lui
+            next_pc_sel   <= 1'b1;
             E_jb_op1_sel <= 1'bx;
             E_alu_op1_sel <= 1'bx;
             E_alu_op2_sel <= 1;
         end
         5'b00101 : begin      // auipc
+            next_pc_sel   <= 1'b1;
             E_jb_op1_sel <= 1'bx;
             E_alu_op1_sel <= 1;
             E_alu_op2_sel <= 1;
         end
         5'b11011 : begin      // jal
+            next_pc_sel   <= 1'b0;
             E_jb_op1_sel <= 1'b1;
             E_alu_op1_sel <= 1;
             E_alu_op2_sel <= 1'bx; 
@@ -163,108 +182,48 @@ always @(*) begin
         default : M_dm_w_en <= 4'd0;
     endcase
 end
+
+always @(*) begin
+    case (W_op)
+        5'b01100 : begin        // R type
+            W_wb_en <= 1;
+            W_wb_data_sel <= 0;
+        end
+        5'b00100 : begin        // immediate
+            W_wb_en <= 1;
+            W_wb_data_sel <= 0;
+        end
+        5'b00000 : begin         // load
+            W_wb_en <= 1;
+            W_wb_data_sel <= 1;
+        end
+        5'b11001 : begin       // jalr
+            W_wb_en <= 1;
+            W_wb_data_sel <= 0;
+        end
+        5'b01000 : begin       // store
+            W_wb_en <= 0;
+            W_wb_data_sel <= 1'bx;
+        end
+        5'b11000 : begin      // branch
+            W_wb_en <= 0;
+            W_wb_data_sel <= 1'bx;
+        end
+        5'b01101 : begin      // lui
+            W_wb_en <= 1;
+            W_wb_data_sel <= 0;
+        end
+        5'b00101 : begin      // auipc
+            W_wb_en <= 1;
+            W_wb_data_sel <= 0;
+        end
+        5'b11011 : begin      // jal
+            W_wb_en <= 1;
+            W_wb_data_sel <= 0;
+        end
+    endcase
+end
 endmodule 
-
-
-// always @(*) begin
-//     case (opcode)
-//         5'b01100 : begin        // R type
-//             next_pc_sel <= 1;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 1'bx;
-//             alu_op1_sel <= 0;
-//             alu_op2_sel <= 0;
-//             wb_sel <= 0;
-//             dm_w_en <= 0;
-//         end
-//         5'b00100 : begin        // immediate
-//             next_pc_sel <= 1;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 1'bx;
-//             alu_op1_sel <= 0;
-//             alu_op2_sel <= 1;
-//             wb_sel <= 0;
-//             dm_w_en <= 0;
-//         end
-//         5'b00000 : begin         // load
-//             next_pc_sel <= 1;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 1'bx;
-//             alu_op1_sel <= 0;
-//             alu_op2_sel <= 1;
-//             wb_sel <= 1;
-//             dm_w_en <= 0;
-//         end
-//         5'b11001 : begin       // jalr
-//             next_pc_sel <= 0;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 0;
-//             alu_op1_sel <= 1;
-//             alu_op2_sel <= 1'bx; 
-//             wb_sel <= 0;
-//             dm_w_en <= 0;
-//         end
-//         5'b01000 : begin       // store
-//             next_pc_sel <= 1;
-//             im_w_en <= 0;
-//             wb_en <= 0;
-//             E_jb_op1_sel <= 1'bx;
-//             alu_op1_sel <= 0;
-//             alu_op2_sel <= 1;
-//             wb_sel <= 1'bx;
-//             case (func3)
-//                 3'b000 : dm_w_en <= 4'b0001; // sb
-//                 3'b001 : dm_w_en <= 4'b0011; // sh
-//                 3'b010 : dm_w_en <= 4'b1111; // sw
-//             endcase
-//         end
-//         5'b11000 : begin      // branch
-//             next_pc_sel <= !b; 
-//             im_w_en <= 0;
-//             wb_en <= 0;
-//             E_jb_op1_sel <= 1'b1;
-//             alu_op1_sel <= 0;
-//             alu_op2_sel <= 0;
-//             wb_sel <= 1'bx;
-//             dm_w_en <= 0;
-//         end
-//         5'b01101 : begin      // lui
-//             next_pc_sel <= 1;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 1'bx;
-//             alu_op1_sel <= 1'bx;
-//             alu_op2_sel <= 1;
-//             wb_sel <= 0;
-//             dm_w_en <= 0;
-//         end
-//         5'b00101 : begin      // auipc
-//             next_pc_sel <= 1;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 1'bx;
-//             alu_op1_sel <= 1;
-//             alu_op2_sel <= 1;
-//             wb_sel <= 0;
-//             dm_w_en <= 0;
-//         end
-//         5'b11011 : begin      // jal
-//             next_pc_sel <= 0;
-//             im_w_en <= 0;
-//             wb_en <= 1;
-//             E_jb_op1_sel <= 1'b1;
-//             alu_op1_sel <= 1;
-//             alu_op2_sel <= 1'bx; 
-//             wb_sel <= 0;
-//             dm_w_en <= 0;
-//         end
-//     endcase
-// end
-// endmodule
 
 /* opcode
 R : 01100
